fb4e6ad0d576f34e9945c3fbb54381f0964276b0
zoqtpdnjs <zpqtpdnjs@gmail.com>
2023-07-02 20:24:23 +0900

    로그인 페이지-2

--- src/main/front/src/component/login/Login.js
+++ src/main/front/src/component/login/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import LoginContent from './LoginContent';
 import LoginHeader from './LoginHeader';
-import './css/gnb.min.css';
-import './css/top.css';
+import '../../css/login/gnb.min.css';
+import '../../css/login/top.css';
 
 const Login = () => {
     return (
